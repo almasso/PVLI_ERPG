02e4c364f02643445c79608d06fa33285b13eb95
DavidRainder <davirive@ucm.es>
2022-10-15 13:08:47 +0200

    Movimiento y Colisiones (manín mal hecho)

--- src/Game.js
+++ src/Game.js
@@ -1,63 +1,44 @@
+import MovementExample from './scenes/exampleMovement.js'
+
 /**
  * Inicio del juego en Phaser. Creamos el archivo de configuración del juego y creamos
  * la clase Game de Phaser, encargada de crear e iniciar el juego.
  */
 let config = {
-    type: Phaser.AUTO,
+    type: Phaser.CANVAS,
+    canvas: document.getElementById("game"),
     width:  800,
     height: 600,
     pixelArt: true,
 	scale: {
-        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY
+        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+		// Configuramos phaser para que se adapte al tamaño de pantalla donde ejecutadmos
+		// con un mínimo y un máximo de tamaño
+		mode: Phaser.Scale.FIT,
+		min: {
+            width: 328,
+            height: 188
+        },
+		max: {
+            width: 1312,
+            height: 752
+        },
+		zoom: 1
     },
-    scene: [{preload:preload, create:create}],
+    scene: [MovementExample],
     physics: { 
         default: 'arcade', 
         arcade: { 
-            gravity: { y: 200 }, 
+            gravity: { y: 0 }, 
             debug: false 
-        } 
+        },
+        checkCollision: {
+            up: true,
+            down: true,
+            left: true,
+            right: true
+        }
     }
 };
 
 new Phaser.Game(config);
-
-// Cargamos los recursos
-function preload (){
-		//Cargamos assets de una URL de otra web
-        //this.load.setBaseURL('http://labs.phaser.io');
-		//this.load.image('sky', 'assets/skies/space.png');
-        //this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-        //this.load.image('red', 'assets/particles/red.png');
-        
-		//Cargamos los assets desde nuestra raiz del proyecto
-		this.load.image('sky', 'assets/space.png');
-        this.load.image('logo', 'assets/phaser3-logo.png');
-        this.load.image('red', 'assets/red.png');
-}
-
-// Añadimos sus efectos
-function create ()
-{
-		//Las imagenes tienen como origen el centro por defecto
-		//this.add.image(400, 300, 'sky');
-		
-		//Imagen con origen superior izquierdo
-		this.add.image(0, 0, 'sky').setOrigin(0, 0);
-        
-        var particles = this.add.particles('red');
-
-        var emitter = particles.createEmitter({
-            speed: 100,
-            scale: { start: 1, end: 0 },
-            blendMode: 'ADD'
-        });
-
-        var logo = this.physics.add.image(400, 100, 'logo');
-
-        logo.setVelocity(100, 200);
-        logo.setBounce(1, 1);
-        logo.setCollideWorldBounds(true);
-
-        emitter.startFollow(logo);
-}
---
+++ src/scenes/exampleMovement.js
@@ -0,0 +1,66 @@
+import Manin from '../obj/manin.js';
+import enviromentObj from '../obj/enviromentObj.js';
+import Bound from '../obj/bound.js';
+
+/**
+ * Escena principal.
+ * @extends Phaser.Scene
+ */
+export default class MovementExample extends Phaser.Scene {
+	
+	constructor() {
+		super({ key: 'movement' });
+	}
+	
+	preload(){
+		this.load.image('manin', 'assets/Manín.png');
+		this.load.image('bg', 'assets/bg.png');
+		this.load.image('house', 'assets/house.png');
+		this.load.image('pixel', 'assets/pixel1x1.png');
+		
+        /*this.load.spritesheet('knight', 'assets/Knight/knight.png', {frameWidth: 72, frameHeight: 86})
+		this.load.spritesheet('box', 'assets/Box/box.png', {frameWidth: 64, frameHeight: 64})*/
+	}
+	
+	/**
+	* Creación de los elementos de la escena principal de juego
+	*/
+    
+	create() {
+
+		//Imagen de fondo
+		var bg = this.add.image(0, 0, 'bg').setOrigin(0, 0);
+        
+
+        this.cameras.main.setBounds(0, 0, bg.displayWidth, bg.displayHeight);
+
+		//Instanciamos nuestro personaje, que es un caballero, y la plataforma invisible que hace de suelo
+		let manin = new Manin(this, 100, 50);
+		let bLeft = new Bound(this, -1, 0,1,bg.displayHeight);
+		let bRight = new Bound(this, bg.displayWidth, 0,1,bg.displayHeight);
+		let bUp = new Bound(this, 0, -1,bg.displayWidth,1);
+		let bDown = new Bound(this, 0, bg.displayHeight,bg.displayWidth,1);
+        this.cameras.main.startFollow(manin);
+        let house = new enviromentObj(this,400,300, 'house');
+
+		let scene = this; // Nos guardamos una referencia a la escena para usarla en la función anidada que viene a continuación
+		
+		manin.body.onCollide = true; // Activamos onCollide para poder detectar la colisión del caballero con el suelo
+
+		this.physics.add.collider(manin, house);
+		this.physics.add.collider(manin, bg);
+		this.physics.add.collider(manin, bLeft);
+		this.physics.add.collider(manin, bDown);
+		this.physics.add.collider(manin, bRight);
+		this.physics.add.collider(manin, bUp);
+
+		/*
+		 * Escuchamos los eventos de colisión en el mundo para poder actuar ante ellos
+		 * En este caso queremos detectar cuando el caballero colisiona con el suelo para activar el salto del personaje
+		 * El salto del caballero lo desactivamos en su "clase" (archivo knight.js) para evitar dobles saltos
+		 * También comprobamos si está en contacto con alguna caja mientras ataca, en ese caso destruimos la caja
+		 */
+
+	}
+
+}
