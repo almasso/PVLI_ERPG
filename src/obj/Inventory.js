91f256b8f8facb4871e3899c1425e65413152cb9
pablosm196 <pablos19@ucm.es>
2022-11-11 18:40:00 +0100

    Inventario

---
+++ src/obj/Inventory.js
@@ -0,0 +1,32 @@
+export class Inventory{
+    constructor(){
+        this.inv =[];
+    }
+
+    addItem(obj){
+        let i = 0;
+        if(this.isItem(obj.name, i)) this.inv[i].quantity++;
+        else this.inv.push(obj);
+    }
+
+    removeItem(obj){
+        let i = 0;
+        if(this.isItem(obj.name, i))
+            this.inv[i].quantity--;
+            if(this.inv[i].quantity === 0){
+                while(i < this.inv.length - 1) {
+                    this.inv[i] = this.inv[i + 1];
+                    i++;
+                }
+                this.inv[i] = undefined;
+                this.inv.length--;
+            }
+    }
+
+    isItem(name, i){
+        while(i < this.inv.length && this.inv[i].name !== name) i++
+        return (i === this.inv.length);
+    }
+}
+
+
---
+++ src/obj/Object.js
@@ -0,0 +1,8 @@
+export class Object{
+    constructor(_name, _hp, _mp){
+        this.name = _name;
+        this.hp = _hp;
+        this.mp = _mp;
+        this.quantity = 0;
+    }
+}
