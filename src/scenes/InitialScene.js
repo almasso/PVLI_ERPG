99a7314177216588a958fcd220c2bbad20c3559a
almasso <99798297+almasso@users.noreply.github.com>
2022-12-15 06:06:34 +0100

    último toquecito a la web

--- src/scenes/InitialScene.js
+++ src/scenes/InitialScene.js
@@ -35,7 +35,7 @@ export default class InitialScene extends Phaser.Scene {
         this.startButton = this.add.image(this.sys.game.canvas.width / 2, this.sys.game.canvas.height / 2, 'startButton').setOrigin(0, 0);
         this.startButton.scale = 4.7;
         this.startButton.x -= this.startButton.displayWidth / 2;
-        this.startButton.y -= this.startButton.displayHeight * 2/ 5 - 2;
+        this.startButton.y -= this.startButton.displayHeight * 2/ 5 + 10;
 
         this.startButton.setInteractive();
         this.startButton.on('pointerup',()=>{
